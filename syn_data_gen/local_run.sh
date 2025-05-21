88a1e928f5aca709d70c04d503483dd20a689fec
Shyamkotainfo <shyamsundar.kota@infoservices.com>
2025-05-21 15:45:26 +0530

    full code update

---
+++ syn_data_gen/local_run.sh
@@ -0,0 +1,5 @@
+# python test_code.py config.yaml test_data.csv
+
+python app/main.py config_files/config.yaml datasets/test_data.csv
+
+generate-synthetic-data syn_data_gen/config_files/config.yaml syn_data_gen/datasets/test_data.csv
