13a27252b96ad5f4fa6f5a4e5ff8354a491e8c81
Shyamharrys <shyamsundar.kota@infoservices.com>
2025-06-01 15:01:14 +0530

    new code

---
+++ syn_data_csv/local_run.sh
@@ -0,0 +1,7 @@
+# python test_code.py config.yaml test_data.csv
+
+python app/main.py ./config_files/config.yaml ./datasets/test_data.csv
+
+uv run syn_data_csv/app/main.py ./config_files/config.yaml ./datasets/test_data.csv
+
+generate-synthetic-data syn_data_gen/config_files/config.yaml syn_data_gen/datasets/test_data.csv
