6e04a4724eef0d425e18837070f149e779c614b7
Shyamharrys <shyamsundar.kota@infoservices.com>
2025-06-01 15:01:41 +0530

    new code

--- app/output.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def generate_ouput(df):
-    if not df.empty:
-        df.to_csv("synthetic_data.csv", index=False)
-        print("✅ Synthetic data saved to synthetic_data.csv")
-    else:
-        print("❌ No valid data to save.")
