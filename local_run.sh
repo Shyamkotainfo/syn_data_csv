2f5bbcaf9bf41963d0acce711fbb01136306d6bc
Shyamharrys <shyamsundar.kota@infoservices.com>
2025-06-17 16:13:13 +0530

    updated new code

--- local_run.sh
+++ local_run.sh
@@ -1,7 +1,7 @@
 # python test_code.py config.yaml test_data.csv
 
-python syn_data_csv/app/main.py ./config_files/config.yaml ./datasets/test_data.csv
+python syn_data_csv/main.py ./config_files/config.yaml ./datasets/test_data.csv
 
 uv run app/main.py ./config_files/config.yaml ./datasets/test_data.csv
 
-generate-synthetic-data syn_data_gen/config_files/config.yaml syn_data_gen/datasets/test_data.csv
+get_synthetic_data syn_data_gen/config_files/integer_data.yaml syn_data_gen/datasets/test_data.csv
